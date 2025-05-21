d24b8e764669d83c2f572b14ce71b12015159569
Paulo Armey <118624556+Paulx09@users.noreply.github.com>
2025-05-21 15:13:41 -0500

    Habilitación de CORS para comunicarse con frontend, implementación del servicio de Gemini y uso de archivos estáticos para el frontend.

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,4 +1,20 @@
+using ECommerceAPI.Models;
+using ECommerceAPI.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add CORS for frontend
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 // Add services to the container.
 
@@ -6,6 +22,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<EcommerceStoreContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+// Servicio de Gemini
+builder.Services.AddScoped<GeminiService>();
 
 var app = builder.Build();
 
@@ -15,6 +38,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowFrontend");
+
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
