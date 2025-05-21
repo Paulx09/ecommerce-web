510dfd0ade52ad517b08214ffb083cf1f8304e11
PaulxA09 <pauloarmey18@gmail.com>
2025-05-21 11:09:39 -0500

    Agregando atributos para las interfaces de la carpeta ordenes.

---
+++ frontend/src/interfaces/ordenes/OrdenDetail.ts
@@ -0,0 +1,8 @@
+export interface OrdenDetail {
+  ordenDetailsId: number;
+  cantidad: number;
+  precioUnit: number;
+  ordenId: number;
+  productoId: number;
+  producto: string;
+}
---
+++ frontend/src/interfaces/ordenes/Ordene.ts
@@ -0,0 +1,9 @@
+export interface Ordene {
+  ordenId: number;
+  fechaOrden: string;
+  estado: string;
+  estadoEmail: string;
+  cliente: string;
+  clienteId: number;
+  metodoPago: string;
+}
