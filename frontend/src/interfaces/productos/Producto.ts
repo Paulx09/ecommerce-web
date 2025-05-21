7fee47b535d998007d3054b9f93ac74ecba69b72
PaulxA09 <pauloarmey18@gmail.com>
2025-05-21 11:09:52 -0500

    Agregando atributos para las interfaces de la carpeta productos.

---
+++ frontend/src/interfaces/productos/Producto.ts
@@ -0,0 +1,10 @@
+export interface Producto {
+    productoId: number;
+    nombre: string;
+    descripcion: string;
+    precio: number;
+    stock: number;
+    disponibilidad: string;
+    imagenNombre: string;
+    categoria: string;
+}
